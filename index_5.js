c591054b0cb3fdc20a6c2616cbfba21662975a36
Artem Degtyarev <artphoto321@gmail.com>
2025-10-30 11:21:18 +0300

    Кроме последнего

---
+++ index_5.js
@@ -0,0 +1,50 @@
+//Задание 1
+/* const myFavoriteFilm = {
+    name: "Зеленая миля",
+    date: 1999,
+    director: "Фрэнк Дарабонт",
+    country: "США"
+}
+
+myFavoriteFilm.revenue = "$286 801 374"
+myFavoriteFilm.getFilmName = function getFilmName() {
+    return myFavoriteFilm.name
+}
+delete myFavoriteFilm.date
+
+console.log(myFavoriteFilm);
+console.log(myFavoriteFilm.getFilmName()); */
+
+/* Задание 2. Реализовать функцию которая будет принимать числовой диапазон в
+качестве параметров [min, max] и будет возвращать случайное целое
+число из данного диапазона. */
+
+/* function randomNumber(min, max){
+let rand = min + Math.random() * (max + 1 - min);
+  return Math.floor(rand);
+}
+console.log(randomNumber(3,60)) */
+
+/* Задание 3. Реализовать функцию которая будет определять, в каком регистре
+записан n элемент переданной строки, если в верхнем то вернуть true, в
+противном случае вернуть false. */
+
+/* let str ="ВаCя Ушел"
+function seachSymbol(n){
+    let m = str[n].toUpperCase();
+    if (str[n]===m){
+    console.log("True");
+    }
+    else {console.log("False")}
+}
+
+seachSymbol(4) */
+
+/* Задание 4. Реализовать функцию которая заменяет в строке str, все вхождения
+подстроки find, на подстроку replace. */
+
+let str="я отец отец"
+
+function replaceStr(find, replace){
+}
+
